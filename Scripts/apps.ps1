3cbf238a8092c3d09b1696046952d7dcb73329a8
beerisgood <beerisgood@users.noreply.github.com>
2020-08-20 11:16:39 +0200

    Removed deletion of important stock Windows Apps
    this would otherwise remove important apps like: new Edge, Paint, Calculator, Photo Viewer, ...

--- Scripts/apps.ps1
+++ Scripts/apps.ps1
@@ -5,15 +5,6 @@
 # https://github.com/stoptracking/windows10
 #
 
-Write-Host "Remove pre-installed applications..."
-Get-AppXProvisionedPackage –online | where-object {$_.PackageName –notlike "*store*"} | Remove-AppxProvisionedPackage -online  -erroraction silentlycontinue
-
-Write-Host "Remove installed applications for all users..."
-Get-AppxPackage -AllUsers | where-object {$_.name –notlike "*store*"} | Remove-AppxPackage -erroraction silentlycontinue
-
-Write-Host "Remove installed applications for the current user..."
-Get-AppxPackage -AllUsers | where-object {$_.name –notlike "*store*"} | Remove-AppxPackage -erroraction silentlycontinue
-
 Write-Host "Disable PowerShell v2"
 Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2
 
