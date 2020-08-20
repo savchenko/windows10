69841210c710f495f1925035f02a29659b95a133
beerisgood <beerisgood@users.noreply.github.com>
2020-08-20 13:24:35 +0200

    Don't disable IPv6
    IPv6 exist since 24 years now and is the future: https://en.wikipedia.org/wiki/IPv6
    Don't block that more and more used protocol. IPv4 is deprecated 
    
    Also Windows has by default activated Privacy Extensions for IPv6: https://tools.ietf.org/html/rfc4941

--- Scripts/interfaces.ps1
+++ Scripts/interfaces.ps1
@@ -9,9 +9,6 @@ Write-Host "Disable NetBios for all interfaces"
 $regkey = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
 Get-ChildItem $regkey | ForEach-Object { Set-ItemProperty -Path "$regkey\$($_.pschildname)" -Name NetbiosOptions -Value 2 -Verbose}
 
-Write-Host "Disable ipv6 for all interfaces"
-Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
-
 Write-Host "Disable LLDP for all interfaces"
 Disable-NetAdapterBinding -Name "*" -ComponentID ms_lldp
 
